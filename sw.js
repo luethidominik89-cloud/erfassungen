1e58f4e6a96e29b3bff9767d08067f92a16cd65a
luethidominik89-cloud <luethidominik89@gmail.com>
2026-08-12 13:26:52 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+/* Offline-Speicher der App. Version hochzählen, wenn eine neue Fassung hochgeladen wird. */
+const VERSION = 'fh-handy-v1';
+const DATEIEN = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(VERSION).then(c => c.addAll(DATEIEN)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks =>
+    Promise.all(ks.filter(k => k !== VERSION).map(k => caches.delete(k)))
+  ).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true }).then(treffer => {
+      if (treffer) {
+        /* im Hintergrund auffrischen, falls Netz da ist */
+        fetch(e.request).then(r => {
+          if (r && r.ok) caches.open(VERSION).then(c => c.put(e.request, r.clone()));
+        }).catch(() => {});
+        return treffer;
+      }
+      return fetch(e.request).then(r => {
+        if (r && r.ok && new URL(e.request.url).origin === location.origin) {
+          const kopie = r.clone();
+          caches.open(VERSION).then(c => c.put(e.request, kopie));
+        }
+        return r;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
